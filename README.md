316331a66085cb7d4a5e3246702b163814e780d0
DavyVan <affqqa@163.com>
2019-06-14 16:41:27 -0700

    Added main.js + index.html

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # YouzimuCC-electron
-A speech to text client
+YouzimuCC is a speech to text client built upon [Elctron](electronjs.org) and [IBM Watson Speech to Text](https://www.ibm.com/watson/services/speech-to-text/) API.  
+It would be used by Youzimu (柚子木字幕组) to improve efficiency of daily work flow.
+
+# Dev Dependencies
+* Node.js
+* Electron
+* Bootstrap
---
+++ main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const {app, BrowserWindow, dialog} = require('electron');
+
+app.on('ready', function () {
+    var mainWin = new BrowserWindow({
+        width: 800,
+        height: 600,
+        resizable: false,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+    mainWin.setMenu(null);
+    mainWin.loadURL('file://' + __dirname + '/app/index.html');
+    mainWin.webContents.openDevTools({mode: "detach"});
+});
